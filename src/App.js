233f73097156dd48878b6f4a2f4c8d11144af564
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-10-26 08:44:01 +0530

    Adding submit function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
+import React, { Component } from 'react';
+import './index.css';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-     bhyyee
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'React Simple CRUD Application',
+      act: 0,
+      index: '',
+      datas: []
+    }
+  } 
+
+  componentDidMount(){
+    this.refs.name.focus();
+  }
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+    
+    console.log('check');
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
+
+    if(this.state.act === 0){   //new
+      let data = {
+        name, address
+      }
+      datas.push(data);
+    }else{                      //update
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].address = address;
+    }    
+
+    this.setState({
+      datas: datas,
+      act: 0
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  
+
+  render() {
+    let datas = this.state.datas;
+    return (
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="name" placeholder="Phone number" className="formField" />
+          <input type="text" ref="address" placeholder="your address" className="formField" />
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+        </form>
+        <pre>
+         
+        </pre>
+      </div>
+    );
+  }
 }
 
 export default App;
